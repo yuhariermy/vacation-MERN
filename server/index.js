7cbe76aec773fd062125571ed3e075c7e336588d
yuhariermy <yuhariermy@gmail.com>
2022-03-26 00:36:25 +0700

    modify index.js

--- server/index.js
+++ server/index.js
@@ -2,3 +2,9 @@ import react from "react";
 import cors from "cors";
 import moongoose from "moongoose";
 import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ extendedlimit: "30mb", extended: true }));
