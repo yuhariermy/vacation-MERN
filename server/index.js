d77d64b5976e4d6d218b83be40b4cf6577de9b3a
yuhariermy <yuhariermy@gmail.com>
2022-03-26 21:41:30 +0700

    adding .env

--- server/index.js
+++ server/index.js
@@ -1,3 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// import "dotenv/config";
+
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
@@ -15,13 +21,11 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ extendedlimit: "30mb", extended: true }));
 
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server running on port : http://localhost:${PORT}`)
