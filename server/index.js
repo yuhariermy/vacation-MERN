f81eb2da93ac62b2ad096f7b5f56378743b2a27d
yuhariermy <yuhariermy@gmail.com>
2022-03-26 20:01:24 +0700

    add new method get and create for Post

--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
-import react from "react";
+import express from "express";
 import cors from "cors";
-import moongoose from "moongoose";
+import mongoose from "mongoose";
 import bodyParser from "body-parser";
 
+// import postRoute from "../server/routes/post";
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
+// app.use("/posts", postRoute);
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ extendedlimit: "30mb", extended: true }));
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(
+    "mongodb+srv://ermiee7:K0d0ksup3r10@cluster0.fp9ur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on port : http://localhost:${PORT}`)
+    )
+  )
+  .catch((err) => console.error(err.message));
